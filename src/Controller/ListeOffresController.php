39b9006f5b95064773d723d46dabc148e695388a
Baptiste Saint-André <baptiste@mkdmpartner.com>
2020-01-07 04:35:22 +0100

    Redirection

--- src/Controller/ListeOffresController.php
+++ src/Controller/ListeOffresController.php
@@ -3,11 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 
 class ListeOffresController extends AbstractController
 {
 
     public function displayListeOffres() {
-        return $this->render('liste-offres.html.twig');
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://9350ba5d.ngrok.io/annonces/_all_docs?include_docs=true');
+        $contents = $response->toArray();
+        // dd($contents);
+        return $this->render('liste-offres.html.twig', [
+            'annonces' => $contents
+        ]);
     }
 }
--- src/Controller/ProfilEntrepriseController.php
+++ src/Controller/ProfilEntrepriseController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 
 class ProfilEntrepriseController extends AbstractController
 {
