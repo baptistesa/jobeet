d0c650c2f0b7727c9c55b89610bf7cbff155fb08
Baptiste Saint-André <baptiste@mkdmpartner.com>
2020-01-07 18:07:16 +0100

    Signup method

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -47,20 +47,29 @@ class HomeController extends AbstractController
         $password = $request->request->get('_password');
         $description = $request->request->get('_description');
 
+        $data = array(
+            'name' => $prenom,
+            'last_name' => $nom,
+            'mail' => $mail,
+            'password' => $password,
+            'description' => $description
+        );
+
         $client = HttpClient::create();
         $response = $client->request('POST', 'https://2ef89cb3.ngrok.io/utilisateurs', [
             "headers" => [
                 "Content-Type" => "application/json"
             ],
-            'body' => [
-                "name" => $prenom,
-                "last_name" => $nom,
-                "mail" => $mail,
-                "password" => $password,
-                "description" => $description
-            ]
+            'body' => json_encode($data)
         ]);
         $contents = $response->getContent();
+        $session = new Session();
+        $session->start();
+        $session->set('mail', $mail);
+        $session->set('name', $prenom);
+        $session->set('last_name', $nom);
+        $session->set('description', $description);
+        $session->set('is_recruteur', false);
         return $this->render('home.html.twig', array('test' => $test));
     }
 
