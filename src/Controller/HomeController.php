6e657873bf2d0fc258809f8974dba25a974841e4
Baptiste Saint-André <baptiste@mkdmpartner.com>
2020-01-07 03:24:54 +0100

    Adding connexion

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,28 +3,50 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpClient\HttpClient;
 
 class HomeController extends AbstractController
 {
 
-    public function displayHome() {
+    public function displayHome(Request $request)
+    {
         $test = 1;
-        return $this->render('home.html.twig', array('test' => $test));
+        $username = $request->request->get('_username');
+        $password = $request->request->get('_password');
+
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://9350ba5d.ngrok.io/utilisateurs/_all_docs?include_docs=true');
+        $contents = $response->toArray();
+
+
+        for ($i = 0; $i < count($contents) - 1; $i++) {
+            $tmp_user = $contents["rows"][$i]["doc"]["mail"];
+            $tmp_password = $contents["rows"][$i]["doc"]["password"];
+            if ($tmp_user == $username && $tmp_password == $password) {
+                return $this->render('home.html.twig', array('test' => $test));
+            }
+        }
+        return $this->render('login.html.twig');
     }
 
-    public function goToLogin() {
+    public function goToLogin()
+    {
         return $this->redirectToRoute('login');
     }
 
-    public function goToRegister() {
+    public function goToRegister()
+    {
         return $this->redirectToRoute('login');
     }
 
-    public function goToProfilentreprise() {
+    public function goToProfilentreprise()
+    {
         return $this->redirectToRoute('profil-entreprise');
     }
-    
-    public function goToListentreprise() {
+
+    public function goToListentreprise()
+    {
         return $this->redirectToRoute('liste-entreprise');
     }
 }
