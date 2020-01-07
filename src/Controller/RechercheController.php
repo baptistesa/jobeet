1ef74fcce79d744b594e19e1148b706311d227ab
joffrey.marc <joffrey.marc@epita.fr>
2020-01-07 19:16:32 +0100

    [Recherche] load data and do template

--- src/Controller/RechercheController.php
+++ src/Controller/RechercheController.php
@@ -3,15 +3,38 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 
 class RechercheController extends AbstractController
 {
 
     public function displayRecherche() {
-        return $this->render('recherche.html.twig');
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://2ef89cb3.ngrok.io/annonces/_all_docs?include_docs=true');
+        $contents = $response->toArray();
+        $respuser = $client->request('GET', 'https://2ef89cb3.ngrok.io/utilisateurs/_all_docs?include_docs=true');
+        $users = $respuser->toArray();
+        $respentreprise = $client->request('GET', 'https://2ef89cb3.ngrok.io/entreprises/_all_docs?include_docs=true');
+        $entreprises = $respentreprise->toArray();
+        return $this->render('recherche.html.twig', [
+            'annonces' => $contents,
+            'users' => $users,
+            'entreprises' => $entreprises
+        ]);
     }
 
     public function search() {
-        return $this->render('recherche.html.twig');
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://2ef89cb3.ngrok.io/annonces/_all_docs?include_docs=true');
+        $contents = $response->toArray();
+        $respuser = $client->request('GET', 'https://2ef89cb3.ngrok.io/utilisateurs/_all_docs?include_docs=true');
+        $users = $respuser->toArray();
+        $respentreprise = $client->request('GET', 'https://2ef89cb3.ngrok.io/entreprises/_all_docs?include_docs=true');
+        $entreprises = $respentreprise->toArray();
+        return $this->render('recherche.html.twig', [
+            'annonces' => $contents,
+            'users' => $users,
+            'entreprises' => $entreprises
+        ]);
     }
 }
