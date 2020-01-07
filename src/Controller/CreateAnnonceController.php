7196acac1230dcf0ad10b10e3602113af071e810
Baptiste Saint-André <baptiste@mkdmpartner.com>
2020-01-07 20:36:46 +0100

    Design + form

---
+++ src/Controller/CreateAnnonceController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+
+class CreateAnnonceController extends AbstractController
+{
+    public function displayForm(Request $request) {
+        return $this->render('create-annonce.html.twig');
+    }
+
+    public function postForm(Request $request)
+    {
+        $test = 1;
+        $prenom = $request->request->get('_prenom');
+        $nom = $request->request->get('_nom');
+        $mail = $request->request->get('_mail');
+        $password = $request->request->get('_password');
+        $description = $request->request->get('_description');
+
+        $data = array(
+            'name' => $prenom,
+            'last_name' => $nom,
+            'mail' => $mail,
+            'password' => $password,
+            'description' => $description
+        );
+
+        $client = HttpClient::create();
+        $response = $client->request('POST', 'https://2ef89cb3.ngrok.io/utilisateurs', [
+            "headers" => [
+                "Content-Type" => "application/json"
+            ],
+            'body' => json_encode($data)
+        ]);
+        $contents = $response->toArray();
+        $session = new Session();
+        $session->start();
+        $session->set('id', $contents["id"]);
+        $session->set('mail', $mail);
+        $session->set('name', $prenom);
+        $session->set('last_name', $nom);
+        $session->set('description', $description);
+        $session->set('is_recruteur', false);
+        
+        return $this->render('home.html.twig', array('test' => $test));
+    }
+}
--- src/Controller/ListeMatchsController.php
+++ src/Controller/ListeMatchsController.php
@@ -8,13 +8,17 @@ use Symfony\Component\HttpClient\HttpClient;
 class ListeMatchsController extends AbstractController
 {
 
-    public function displayListeMatchs() {
-        $client = HttpClient::create();
-        $response = $client->request('GET', 'https://2ef89cb3.ngrok.io/matches/_all_docs?include_docs=true');
-        $contents = $response->toArray();
+    public function displayListeMatchs()
+    {
+        // $client = HttpClient::create();
+        // $response = $client->request('GET', 'https://2ef89cb3.ngrok.io/matches/_all_docs?include_docs=true');
+        // $contents = $response->toArray();
         // dd($contents);
-        return $this->render('liste-match.html.twig', [
-            'matches' => $contents
+        // return $this->render('liste-match.html.twig', [
+        //     'matches' => $contents
+        // ]);
+        return $this->render('liste-matchs.html.twig', [
+            "matches" => null
         ]);
     }
 }
