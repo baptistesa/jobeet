92b0825abf3befe7a1ca6bd0fb4c2ef2b9e1a664
joffrey.marc <joffrey.marc@epita.fr>
2020-01-07 03:27:01 +0100

    [ProfilEntreprise] Try with params

--- src/Controller/ProfilEntrepriseController.php
+++ src/Controller/ProfilEntrepriseController.php
@@ -7,7 +7,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ProfilEntrepriseController extends AbstractController
 {
 
-    public function displayProfilEntreprise() {
-        return $this->render('profil-entreprise.html.twig');
+    public function displayProfilEntreprise($id) {
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://9350ba5d.ngrok.io/entreprises/_all_docs?include_docs=true');
+        $contents = $response->toArray();
+        return $this->render('profil-entreprise.html.twig', [ 
+            'entreprises' => $contents,
+            'id' => $id
+        ]);
     }
 }
