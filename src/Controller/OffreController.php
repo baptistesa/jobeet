2c0f879f0b60478009b815d890428575186cc1a4
joffrey.marc <joffrey.marc@epita.fr>
2020-01-12 01:29:37 +0100

    [Offre] handle 'inviter' button

--- src/Controller/OffreController.php
+++ src/Controller/OffreController.php
@@ -14,9 +14,24 @@ class OffreController extends AbstractController
         $contents = $response->toArray();
         $respuser = $client->request('GET', 'https://20678575.ngrok.io/utilisateurs/_all_docs?include_docs=true');
         $users = $respuser->toArray();
+        $annonce_finale = null;
+        $invited = [];
+        foreach ($contents["rows"] as $annonce)
+        {
+            if ($annonce["doc"]["_id"] == $id)
+            {
+                $annonce_finale = $annonce;
+                foreach ($annonce["doc"]["matches"] as $matchs)
+                {
+                    if (in_array($matchs["id_user"], $annonce["doc"]["postulants"]) == false)
+                        $invited[] = $matchs["id_user"];
+                }
+            }
+        }
         return $this->render('offre.html.twig', [
             'annonces' => $contents,
             'users' => $users,
+            'inviteds' => $invited,
             'id' => $id
         ]);
     }
