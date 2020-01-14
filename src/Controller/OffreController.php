ba9c8eafbbbc0914e038f127bcb19a8f399b4af3
Baptiste Saint-André <baptiste@mkdmpartner.com>
2020-01-14 03:05:34 +0100

    Adding reject invitation

--- src/Controller/OffreController.php
+++ src/Controller/OffreController.php
@@ -128,7 +128,47 @@ class OffreController extends AbstractController
         ]);
     }
 
-    public function refuser()
+    public function refuser($id, $offre_id)
     {
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://d10080de.ngrok.io/annonces/_all_docs?include_docs=true');
+        $contents = $response->toArray();
+        $respuser = $client->request('GET', 'https://d10080de.ngrok.io/utilisateurs/_all_docs?include_docs=true');
+        $users = $respuser->toArray();
+        $annonce_finale = null;
+        $invited = [];
+
+        $session = new Session();
+        $session->start();
+
+        foreach ($contents["rows"] as $annonce) {
+            if ($annonce["doc"]["_id"] == $offre_id) {
+                if (in_array($id, $annonce["doc"]["postulants_refuses"]) == false)
+                    array_push($annonce["doc"]["postulants_refuses"], $id);
+                $annonce_finale = $annonce;
+                foreach ($annonce["doc"]["matches"] as $matchs) {
+                    if (in_array($matchs["id_user"], $annonce["doc"]["postulants"]) == false)
+                        $invited[] = $matchs["id_user"];
+                }
+                $response_update = $client->request('PUT', 'https://d10080de.ngrok.io/annonces/' . $offre_id, [
+                    "headers" => [
+                        "Content-Type" => "application/json"
+                    ],
+                    'body' => json_encode($annonce["doc"])
+                ]);
+                $contents_update = $response_update->toArray();
+
+                $response = $client->request('GET', 'https://d10080de.ngrok.io/annonces/_all_docs?include_docs=true');
+                $contents = $response->toArray();
+            }
+        }
+
+        return $this->render('offre.html.twig', [
+            'annonces' => $contents,
+            'users' => $users,
+            'inviteds' => $invited,
+            'id' => $offre_id,
+            'is_recruteur' => $session->get('is_recruteur')
+        ]);
     }
 }
