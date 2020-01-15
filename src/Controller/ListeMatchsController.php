03aeb29836fb478b9159905fcce9e530e182ace1
joffrey.marc <joffrey.marc@epita.fr>
2020-01-15 19:10:51 +0100

    [TRier ] can sort by pertinance list of match

--- src/Controller/ListeMatchsController.php
+++ src/Controller/ListeMatchsController.php
@@ -24,7 +24,7 @@ class ListeMatchsController extends AbstractController
         $client = HttpClient::create();
         $response = $client->request('GET', 'https://3296c880.ngrok.io/annonces/_all_docs?include_docs=true');
         $contents = $response->toArray();
-
+        $sort_matchs = [];
         $annonces = [];
         for ($i = 0; $i < count($contents["rows"]); $i++) {
             for ($j = 0; $j < count($contents["rows"][$i]["doc"]["matches"]); $j++) {
@@ -33,6 +33,45 @@ class ListeMatchsController extends AbstractController
                 }
             }
         }
+
+
+
+
+
+        return $this->render('liste-matchs.html.twig', array("is_recruteur" => false, "annonces" => $annonces));
+    }
+
+    public function compare($a, $b)
+    {
+        if ($a["percent_match"] == $b["percent_match"]) {
+            return 0;
+        }
+        return ($a["percent_match"] < $b["percent_match"]) ? -1 : 1;
+    }
+
+    public function displaysortUser()
+    {
+        $session = new Session();
+        $session->start();
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://3296c880.ngrok.io/annonces/_all_docs?include_docs=true');
+        $contents = $response->toArray();
+        $sort_matchs = [];
+        $annonces = [];
+        for ($i = 0; $i < count($contents["rows"]); $i++) {
+            for ($j = 0; $j < count($contents["rows"][$i]["doc"]["matches"]); $j++) {
+                if ($contents["rows"][$i]["doc"]["matches"][$j]["id_user"] == $session->get("id")) {
+                    array_push($annonces, (object) ['annonce' => $contents["rows"][$i]["doc"], 'percent_match' => $contents["rows"][$i]["doc"]["matches"][$j]["percent_match"]]);
+                }
+            }
+        }
+
+        usort($annonces, function ($a, $b) {
+            if($a->percent_match == $b->percent_match)
+                return 0;
+            return ($a->percent_match > $b->percent_match) ? -1 : 1;
+        });
+
         return $this->render('liste-matchs.html.twig', array("is_recruteur" => false, "annonces" => $annonces));
     }
 
