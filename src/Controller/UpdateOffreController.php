5b886c823229e5400cddcc0b91abe1ed1ff12bed
Baptiste Saint-André <baptiste@mkdmpartner.com>
2020-01-15 16:44:47 +0100

    Update match when updating offer

--- src/Controller/UpdateOffreController.php
+++ src/Controller/UpdateOffreController.php
@@ -31,7 +31,7 @@ class UpdateOffreController extends AbstractController
 
 
 
-        $response2 = $client->request('GET', 'https://3296c880.ngrok.io/annonces/_all_docs?include_docs=true');
+        $response2 = $client->request('GET', 'https://3296c880.ngrok.io/utilisateurs/_all_docs?include_docs=true');
         $contents2 = $response2->toArray();
 
 
@@ -39,7 +39,8 @@ class UpdateOffreController extends AbstractController
 
         $title = "";
         $description = "";
-        $competences = "";
+        if ($request->request->get('_competences'))
+            $competences = explode(' ', $request->request->get('_competences'));
 
         for ($l = 0; $l < count($contents["rows"]); $l++) {
             if ($contents["rows"][$l]["doc"]["_id"] == $offre_id) {
@@ -50,6 +51,30 @@ class UpdateOffreController extends AbstractController
                 if ($request->request->get('_competences'))
                     $contents["rows"][$l]["doc"]["competences"] = explode(' ', $request->request->get('_competences'));
 
+
+
+                $matches = [];
+
+                $total_competences = count($competences);
+                for ($i = 0; $i < count($contents2["rows"]); ++$i) {
+                    $current_id_user = $contents2["rows"][$i]["doc"]["_id"];
+
+                    $comepetences_done = 0;
+                    for ($j = 0; $j < count($contents2["rows"][$i]["doc"]["competences"]); $j++) {
+                        for ($k = 0; $k < count($competences); $k++) {
+                            if ($contents2["rows"][$i]["doc"]["competences"][$j] == $competences[$k])
+                                $comepetences_done++;
+                        }
+                    }
+                    $percent_match = $comepetences_done * 100 / $total_competences;
+                    if ($percent_match >= 75 && $contents2["rows"][$i]["doc"]["is_actif"]) {
+                        array_push($matches, (object) ['id_user' => $current_id_user, 'percent_match' => $percent_match]);
+                    }
+                }
+                $contents["rows"][$l]["doc"]["matches"] = $matches;
+
+                // var_dump($matches);
+
                 $response_update = $client->request('PUT', 'https://3296c880.ngrok.io/annonces/' . $offre_id, [
                     "headers" => [
                         "Content-Type" => "application/json"
@@ -58,27 +83,7 @@ class UpdateOffreController extends AbstractController
                 ]);
                 $contents_update = $response_update->toArray();
 
-
-                // $matches = [];
-
-                // $total_competences = count($competences);
-                // for ($i = 0; $i < count($contents2["rows"]); ++$i) {
-                //     $current_id_user = $contents2["rows"][$i]["doc"]["_id"];
-
-                //     $comepetences_done = 0;
-                //     for ($j = 0; $j < count($contents2["rows"][$i]["doc"]["competences"]); $j++) {
-                //         for ($k = 0; $k < count($competences); $k++) {
-                //             if ($contents2["rows"][$i]["doc"]["competences"][$j] == $competences[$k])
-                //                 $comepetences_done++;
-                //         }
-                //     }
-                //     $percent_match = $comepetences_done * 100 / $total_competences;
-                //     if ($percent_match >= 75 && $contents2["rows"][$i]["doc"]["is_actif"]) {
-                //         array_push($matches, (object) ['id_user' => $current_id_user, 'percent_match' => $percent_match]);
-                //     }
-                // }
-
-
+                
             }
         }
         return $this->render('update-annonce.html.twig', [
